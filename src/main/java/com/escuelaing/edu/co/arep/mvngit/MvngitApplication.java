99fc0a3997fb01bd7d046e3a37931dce69d4c212
Juan Sebastian <Juan_munozd@hotmail.es>
2021-08-17 19:28:13 -0500

    Primer Commit

---
+++ src/main/java/com/escuelaing/edu/co/arep/mvngit/MvngitApplication.java
@@ -0,0 +1,13 @@
+package com.escuelaing.edu.co.arep.mvngit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvngitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvngitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escuelaing/edu/co/arep/mvngit/MvngitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuelaing.edu.co.arep.mvngit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvngitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
