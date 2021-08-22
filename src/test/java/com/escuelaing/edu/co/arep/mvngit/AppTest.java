6c483fc33cec1934e7cd43f1f8a728d5f674b568
Juan Sebastian <Juan_munozd@hotmail.es>
2021-08-22 15:48:57 -0500

    Fifth Commit (TDD included)

--- src/test/java/com/escuelaing/edu/co/arep/mvngit/AppTest.java
+++ src/test/java/com/escuelaing/edu/co/arep/mvngit/AppTest.java
@@ -1,5 +1,7 @@
 package com.escuelaing.edu.co.arep.mvngit;
 
+import com.escuelaing.edu.co.arep.mvngit.entity.ActionResponseApi;
+import java.io.IOException;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +9,98 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
+    
+    ActionResponseApi actionResponseApi;
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testActionResponseApiDay() {
+        
+        String name = "MSFT";
+        String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + name + "&apikey=CX3DG08ISX9HLJCV";
+        
+        actionResponseApi = new ActionResponseApi(name, url);
+        
+        try{
+            actionResponseApi.consultar();
+            assertTrue(true);
+        }catch(IOException e){
+            assertTrue(false);
+        }
+
+        assertTrue(actionResponseApi.getResponseApiData().toString().contains("Meta Data"));
     }
+
+    public void testActionResponseApiWeek() {
+        
+        String name = "MSFT";
+        String url = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" + name + "&apikey=CX3DG08ISX9HLJCV";
+        
+        actionResponseApi = new ActionResponseApi(name, url);
+        
+        try{
+            actionResponseApi.consultar();
+            assertTrue(true);
+        }catch(IOException e){
+            assertTrue(false);
+        }
+
+        assertTrue(actionResponseApi.getResponseApiData().toString().contains("Meta Data"));
+    }   
+    
+    public void testActionResponseApiMonth() {
+        
+        String name = "MSFT";
+        String url = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" + name + "&apikey=CX3DG08ISX9HLJCV";
+        
+        actionResponseApi = new ActionResponseApi(name, url);
+        
+        try{
+            actionResponseApi.consultar();
+            assertTrue(true);
+        }catch(IOException e){
+            assertTrue(false);
+        }
+
+        assertTrue(actionResponseApi.getResponseApiData().toString().contains("Meta Data"));
+    }    
+    
+    public void testActionResponseApiIntraDay() {
+        
+        String name = "MSFT";
+        String url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + name + "&interval=1min&apikey=CX3DG08ISX9HLJCV";
+        
+        
+        actionResponseApi = new ActionResponseApi(name, url);
+        
+        try{
+            actionResponseApi.consultar();
+            assertTrue(true);
+        }catch(IOException e){
+            assert(false);
+        }
+
+        assertTrue(actionResponseApi.getResponseApiData().toString().contains("Meta Data"));
+    }    
+
+
 }
